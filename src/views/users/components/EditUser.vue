7d0031eeee6c3b9159d3eea5d2b3ddc442d93493
Gahon <1053475617@qq.com>
2019-06-07 15:55:55 +0800

    添加用户编辑模块

---
+++ src/views/users/components/EditUser.vue
@@ -0,0 +1,217 @@
+<template>
+  <div
+    v-if="postForm != null"
+    class="edit-user"
+  >
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogFormVisible"
+    >
+      <el-form
+        ref="form"
+        :model="postForm"
+        label-width="130px"
+      >
+
+        <el-form-item label="用户名: ">
+          <div v-if="action === 'register'">
+            <el-input
+              v-model="postForm.name"
+              class="edit-input"
+            />
+          </div>
+          <span
+            v-else
+            class="label"
+          >
+            {{ postForm.name }}
+          </span>
+        </el-form-item>
+
+        <el-form-item label="密码: ">
+          <el-input
+            v-model="postForm.pwd"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item label="性别: ">
+
+          <el-input
+            v-model="postForm.gender"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item label="邮箱: ">
+
+          <el-input
+            v-model="postForm.email"
+            class="edit-input"
+          />
+
+        </el-form-item>
+
+        <el-form-item label="电话: ">
+
+          <el-input
+            v-model="postForm.phone"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item label="dept: ">
+          <el-input
+            v-model="postForm.dept"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item label="grade: ">
+          <el-input
+            v-model="postForm.grade"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item label="language: ">
+          <el-input
+            v-model="postForm.language"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item label="region: ">
+          <template v-if="action === 'register'">
+            <el-input
+              v-model="postForm.region"
+              class="edit-input"
+              size="small"
+            />
+          </template>
+          <span
+            v-else
+            class="label"
+          >
+            {{ postForm.region }}
+          </span>
+        </el-form-item>
+        <el-form-item label="role: ">
+          <el-input
+            v-model="postForm.role"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item label="preferTags: ">
+          <el-input
+            v-model="postForm.preferTags"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item label="obtainedCredits: ">
+          <el-input
+            v-model="postForm.obtainedCredits"
+            class="edit-input"
+          />
+
+        </el-form-item>
+        <el-form-item
+          v-if="action !== 'register'"
+          label="timestamp: "
+        >
+          <span class="label">
+            {{ postForm.timestamp }}
+          </span>
+        </el-form-item>
+      </el-form>
+      <div
+        slot="footer"
+        class="dialog-footer"
+      >
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button
+          type="primary"
+          @click="Submit"
+        >确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+  <!-- <div v-else>
+    <el-alert
+      title="传入数据错误"
+      type="error"
+      description="编辑所需要的数据不能为空"
+      show-icon
+    />
+  </div> -->
+</template>
+
+<script>
+export default {
+  name: 'EditUser',
+  props: {
+    visiable: {
+      type: Boolean,
+      default: false
+    },
+    postForm: {
+      type: Object,
+      default() {
+        return {
+          uid: '',
+          name: '',
+          pwd: '',
+          gender: '',
+          email: '',
+          phone: '',
+          dept: '',
+          grade: '',
+          language: '',
+          region: '',
+          role: '',
+          preferTags: '',
+          obtainedCredits: '',
+          timestamp: null
+        }
+      }
+    },
+    action: {
+      type: String,
+      default: 'register'
+    },
+    title: {
+      type: String,
+      default: '注册'
+    }
+  },
+  data() {
+    return {
+      edit: false
+    }
+  },
+  computed: {
+    dialogFormVisible: {
+      get() {
+        return this.visiable
+      },
+      set(val) {
+        this.$emit('update:visiable', val)
+      }
+    }
+  },
+  methods: {
+    Submit() {
+      this.dialogFormVisible = false
+      this.$emit('submit', this.postForm)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.edit-input {
+  width: 180px;
+}
+</style>
--- src/views/users/index.vue
+++ src/views/users/index.vue
@@ -1,205 +1,220 @@
 <template>
-  <div
-    class="app-container"
-    align="center"
-  >
-    <el-tabs
-      v-model="listQuery.dbms"
-      :stretch="true"
-      style="margin-bottom: 20px; width: 50%; align=center"
-      @tab-click="handleClick"
+  <div>
+    <div
+      class="app-container"
+      align="center"
     >
-      <el-tab-pane
-        label="Beijing"
-        name="Beijing"
-      />
-      <el-tab-pane
-        label="Hong Kong"
-        name="Hong Kong"
-      />
-    </el-tabs>
-
-    <section id="search-title">
-      <el-form
-        :inline="true"
-        :model="listQuery"
-        class="demo-form-inline"
+      <el-tabs
+        v-model="listQuery.dbms"
+        :stretch="true"
+        style="margin-bottom: 20px; width: 50%; align=center"
+        @tab-click="handleClick"
       >
-        <el-form-item label="用户名:">
-          <el-input
-            v-model="listQuery.name"
-            type="text"
-            style="width:150px"
-            placeholder="请输入用户名"
-          />
-        </el-form-item>
-        <el-form-item label="性别:">
-          <el-select
-            v-model="listQuery.gender"
-            placeholder="性别"
-            style="width:100px"
-          >
-            <el-option
-              label="全部"
-              :value="null"
-            />
-            <el-option
-              label="男"
-              value="male"
-            />
-            <el-option
-              label="女"
-              value="female"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item
-          id="submit-item"
-          style="margin-left:20px"
-        >
-          <el-button
-            type="primary"
-            @click="onSearchSubmit"
-          >查询</el-button>
-        </el-form-item>
-        <el-form-item
-          id="clear-item"
-          class="fr"
-        >
-          <el-button
-            type="warning"
-            @click="resetSearch"
-          >重置</el-button>
-        </el-form-item>
-        <el-form-item
-          id="addNew-item"
-          class="fr"
-        >
-          <el-button
-            type="primary"
-            @click="addNewHandler"
-          >新增</el-button>
-        </el-form-item>
-      </el-form>
-    </section>
+        <el-tab-pane
+          label="Beijing"
+          name="Beijing"
+        />
+        <el-tab-pane
+          label="Hong Kong"
+          name="Hong Kong"
+        />
+      </el-tabs>
 
-    <el-table
-      v-loading="listLoading"
-      :data="list"
-      element-loading-text="Loading"
-      stripe
-      highlight-current-row
-    >
-      <el-table-column
-        type="index"
-        width="50"
-      />
-      <!-- <el-table-column align="center" label="ID" width="80">
+      <section id="search-title">
+        <el-form
+          :inline="true"
+          :model="listQuery"
+          class="demo-form-inline"
+        >
+          <el-form-item label="用户名:">
+            <el-input
+              v-model="listQuery.name"
+              type="text"
+              style="width:150px"
+              placeholder="请输入用户名"
+            />
+          </el-form-item>
+          <el-form-item label="性别:">
+            <el-select
+              v-model="listQuery.gender"
+              placeholder="性别"
+              style="width:100px"
+            >
+              <el-option
+                label="全部"
+                :value="null"
+              />
+              <el-option
+                label="男"
+                value="male"
+              />
+              <el-option
+                label="女"
+                value="female"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item
+            id="submit-item"
+            style="margin-left:20px"
+          >
+            <el-button
+              type="primary"
+              @click="onSearchSubmit"
+            >查询</el-button>
+          </el-form-item>
+          <el-form-item
+            id="clear-item"
+            class="fr"
+          >
+            <el-button
+              type="warning"
+              @click="resetSearch"
+            >重置</el-button>
+          </el-form-item>
+          <el-form-item
+            id="addNew-item"
+            class="fr"
+          >
+            <el-button
+              type="primary"
+              @click="addNewHandler"
+            >新增</el-button>
+          </el-form-item>
+        </el-form>
+      </section>
+
+      <el-table
+        v-loading="listLoading"
+        :data="list"
+        element-loading-text="Loading"
+        stripe
+        highlight-current-row
+      >
+        <el-table-column
+          type="index"
+          width="50"
+        />
+        <!-- <el-table-column align="center" label="ID" width="80">
         <template slot-scope="scope">{{ scope.row.uid }}</template>
       </el-table-column> -->
-      <el-table-column
-        label="姓名"
-        width="110"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.name }}</template>
-      </el-table-column>
-      <el-table-column
-        label="性别"
-        width="80"
-        align="center"
-      >
-        <template slot-scope="scope">
-          <a v-if="scope.row.gender === 'male'">男</a>
-          <a v-else>女</a>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="电话"
-        width="200"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.phone }}</template>
-      </el-table-column>
-      <el-table-column
-        label="邮件"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.email }}</template>
-      </el-table-column>
-      <el-table-column
-        label="dept"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.dept }}</template>
-      </el-table-column>
-      <el-table-column
-        label="grade"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.grade }}</template>
-      </el-table-column>
-      <el-table-column
-        label="语言"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.language }}</template>
-      </el-table-column>
-      <el-table-column
-        label="region"
-        align="center"
-        width="100px"
-      >
-        <template slot-scope="scope">{{ scope.row.region }}</template>
-      </el-table-column>
-      <el-table-column
-        label="role"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.role }}</template>
-      </el-table-column>
-      <el-table-column
-        label="Tags"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.preferTags }}</template>
-      </el-table-column>
-      <el-table-column
-        label="Credits"
-        align="center"
-      >
-        <template slot-scope="scope">{{ scope.row.obtainedCredits }}</template>
-      </el-table-column>
-      <el-table-column
-        align="center"
-        prop="created_at"
-        width="200"
-        label="注册时间"
-      >
-        <template slot-scope="scope">
-          <i class="el-icon-time" />
-          <span>{{ scope.row.timestamp }}</span>
-        </template>
-      </el-table-column>
-      <el-table-column
-        label="操作"
-        width="80"
-      >
-        <template slot-scope="scope">
-          <el-button
-            size="mini"
-            type="danger"
-            @click="handleDelete(scope.row)"
-          >删除</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <pagination
-      v-show="total / listQuery.size > 1"
-      :total="total"
-      :page.sync="listQuery.page"
-      :limit.sync="listQuery.size"
-      @pagination="fetchUsers"
+        <el-table-column
+          label="姓名"
+          width="110"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.name }}</template>
+        </el-table-column>
+        <el-table-column
+          label="性别"
+          width="80"
+          align="center"
+        >
+          <template slot-scope="scope">
+            <a v-if="scope.row.gender === 'male'">男</a>
+            <a v-else>女</a>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="电话"
+          width="200"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.phone }}</template>
+        </el-table-column>
+        <el-table-column
+          label="邮件"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.email }}</template>
+        </el-table-column>
+        <el-table-column
+          label="dept"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.dept }}</template>
+        </el-table-column>
+        <el-table-column
+          label="grade"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.grade }}</template>
+        </el-table-column>
+        <el-table-column
+          label="语言"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.language }}</template>
+        </el-table-column>
+        <el-table-column
+          label="region"
+          align="center"
+          width="100px"
+        >
+          <template slot-scope="scope">{{ scope.row.region }}</template>
+        </el-table-column>
+        <el-table-column
+          label="role"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.role }}</template>
+        </el-table-column>
+        <el-table-column
+          label="Tags"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.preferTags }}</template>
+        </el-table-column>
+        <el-table-column
+          label="Credits"
+          align="center"
+        >
+          <template slot-scope="scope">{{ scope.row.obtainedCredits }}</template>
+        </el-table-column>
+        <el-table-column
+          align="center"
+          prop="created_at"
+          width="200"
+          label="注册时间"
+        >
+          <template slot-scope="scope">
+            <i class="el-icon-time" />
+            <span>{{ scope.row.timestamp }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column
+          label="操作"
+          width="160"
+        >
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              type="success"
+              @click="handleEdit(scope.row)"
+            >编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.row)"
+            >删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <pagination
+        v-show="total / listQuery.size > 1"
+        :total="total"
+        :page.sync="listQuery.page"
+        :limit.sync="listQuery.size"
+        @pagination="fetchUsers"
+      />
+
+    </div>
+    <edit-user
+      :visiable.sync="dialogVisiable"
+      :post-form="dialogUser"
+      :action="dialogAction"
+      :title="dialogTitle"
+      @submit="HandleSubmit"
     />
   </div>
 </template>
@@ -207,9 +222,10 @@
 <script>
 import { getUsers, deleteUser } from '@/api/admin.js'
 import Pagination from '@/components/Pagination' // Secondary package based on el-pagination
+import EditUser from './components/EditUser'
 
 export default {
-  components: { Pagination },
+  components: { Pagination, EditUser },
   filters: {
     statusFilter(status) {
       const statusMap = {
@@ -222,6 +238,10 @@ export default {
   },
   data() {
     return {
+      dialogAction: 'edit',
+      dialogVisiable: false,
+      dialogUser: null,
+      dialogTitle: '',
       total: 0,
       activeName: 'Beijing',
       list: null,
@@ -239,13 +259,44 @@ export default {
     this.fetchUsers()
   },
   methods: {
+    HandleSubmit(info) {
+      console.log(this.dialogUser)
+      for (const v of this.list) {
+        if (v.uid === info.uid) {
+          const index = this.list.indexOf(v)
+          this.list.splice(index, 1, info)
+          break
+        }
+      }
+    },
     resetSearch() {
       this.listQuery.name = null
       this.listQuery.gender = null
       this.listQuery.page = 1
       this.fetchUsers()
     },
-    addNewHandler() {},
+    addNewHandler() {
+      var info = {
+        uid: '',
+        name: '',
+        pwd: '',
+        gender: '',
+        email: '',
+        phone: '',
+        dept: '',
+        grade: '',
+        language: '',
+        region: '',
+        role: '',
+        preferTags: '',
+        obtainedCredits: '',
+        timestamp: null
+      }
+      this.dialogUser = info
+      this.dialogTitle = '添加新用户'
+      this.dialogAction = 'register'
+      this.dialogVisiable = true
+    },
     onSearchSubmit() {
       for (var prop in this.listQuery) {
         if (this.listQuery[prop] === '') {
@@ -268,6 +319,13 @@ export default {
         this.total = response.data.total
         this.listLoading = false
       })
+    },
+    handleEdit(row) {
+      this.dialogUser = Object.assign({}, row)
+      // console.log(this.dialogUser)
+      this.dialogAction = 'edit'
+      this.dialogTitle = '编辑用户信息'
+      this.dialogVisiable = true
     },
     handleDelete(row) {
       this.$confirm('确定删除该需求?', '删除确认', {
